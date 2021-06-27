d8d87d92b9212bd5ba2a4adf37ea17636f1aa3de
pablodamascenoo <pablofacex@gmail.com>
2021-06-27 13:48:47 -0300

    concluído find e find-if

---
+++ find_if/include/find_if.h
@@ -0,0 +1,29 @@
+#ifndef GRAAL_H
+#define GRAAL_H
+
+#include <utility>
+using std::pair;
+#include <iterator>
+using std::distance;
+#include <algorithm>
+using std::sort;
+
+namespace graal {
+
+/*! 
+ * TODO: documentação no estilo doxygen
+ */
+template<class InputIt, class UnaryPredicate>
+InputIt find_if(InputIt first, InputIt last, UnaryPredicate p)
+{
+    // TODO
+    for(InputIt i = first; i<last; i++){
+        if(p(*i)==true){
+            return i;
+        }
+    }
+    return last;
+}
+
+}
+#endif
